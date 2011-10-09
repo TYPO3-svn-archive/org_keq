3d9feaf309bc82025ecea864f7905d2cf2791311
uherrmann <uherrmann@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-09 15:54:12 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_keq/trunk@52842 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -4,6 +4,7 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 
+
   // Store record configuration
 $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
 switch($confArr['store_records']) {
@@ -14,22 +15,18 @@ case('Clear presented: each record group in one directory at most'):
 	$str_store_record_conf = 'IN (###PAGE_TSCONFIG_ID###)';
 	break;
 case('Easy 2: same as easy 1 but with storage pid'):
-	$str_marker_pid        = '###STORAGE_PID###';
 	$str_store_record_conf = '= ###STORAGE_PID###';
 	break;
 case('Easy 1: all in the same directory'):
 	default:
-	$str_store_record_conf = 'pid=###CURRENT_PID###';
+	$str_store_record_conf = '= ###CURRENT_PID###';
 }
 
 
 
-	//  add workshop relation to ke_questionnaire.result
-	//  @ToDo: if necessary add other relations
-	//  @ToDo: maybe it should be configurable in EM
-t3lib_div::loadTCA('tx_org_workshop');
-$tempColumns = array (
-	'tx_orgkeq_tx_org_workshop' => array (		
+	//  ke_questionnaire.result: add workshop relation
+$tempColumns = array(
+	'tx_orgkeq_tx_org_workshop' => array(
 		'exclude' => 1,		
 		'label'   => 'LLL:EXT:org_keq/locallang_db.xml:tx_kequestionnaire_results.tx_orgkeq_tx_org_workshop',
 		'config'  => array (
@@ -39,7 +36,7 @@ $tempColumns = array (
 			'size'                => 1,
 			'minitems'            => 0,
 			'maxitems'            => 1,
-			'items'               => Array (
+			'items'               => array(
 				array('', ''),
 			),
 		),
@@ -48,8 +45,12 @@ $tempColumns = array (
 
 t3lib_div::loadTCA('tx_kequestionnaire_results');
 $TCA['tx_kequestionnaire_results']['ctrl']['dividers2tabs'] = 1;
+$TCA['tx_kequestionnaire_results']['interface']['showRecordFieldList'] .= ',tx_orgkeq_tx_org_workshop';
 t3lib_extMgm::addTCAcolumns('tx_kequestionnaire_results', $tempColumns, 1);
 t3lib_extMgm::addToAllTCAtypes('tx_kequestionnaire_results', '--div--;LLL:EXT:org_keq/locallang_db.xml:tx_kequestionnaire_results.div_relations,tx_orgkeq_tx_org_workshop;;;;1-1-1');
 
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/workshops_rating/', '+Org Workshop: Rating');
+
+
+	//  add static files
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/workshops_rating/', '+Org Workshop: Rating');
 ?>
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,15 +1,6 @@
-#
-# Table structure for table 'tx_kequestionnaire_results_tx_orgkeq_tx_org_workshop_mm'
-# 
-#
-#CREATE TABLE tx_kequestionnaire_results_tx_orgkeq_tx_org_workshop_mm (
-#  uid_local int(11) DEFAULT '0' NOT NULL,
-#  uid_foreign int(11) DEFAULT '0' NOT NULL,
-#  tablenames varchar(30) DEFAULT '' NOT NULL,
-#  sorting int(11) DEFAULT '0' NOT NULL,
-#  KEY uid_local (uid_local),
-#  KEY uid_foreign (uid_foreign)
-#);
+# INDEX
+# -----
+# tx_kequestionnaire_results
 
 
 
